e426c08caabfb9b60991fee98a21dec423cacabf
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2022-07-28 13:33:17 +0530

    BUTTON CLICK MAP

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,13 +1,17 @@
 import { Button } from '@material-ui/core'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Item = (product) => {
     const [category, setCategory]=useState([])
-    filterProduct(()=>{
-        product.product.map((cat)=>(
-            setCategory(cat.category)
-        ))
-    })
+   
+
+    useEffect(()=>{
+       const filterProduct = (()=>{
+            product.product.map((cat)=>(
+                setCategory(cat.category)
+            ))
+        })
+    },[])
   return (
     <>
     
@@ -15,7 +19,7 @@ const Item = (product) => {
 {
     product.product.map((item)=>(
         <div key={item.id}>
-            <Button className='btn-secondary' onclick={filterProduct()}>jh
+            <Button className='btn-secondary' onClick={() =>{setCategory(category)}}>jh
                </Button>
                 HI---------------
             {item.title}{item.description}<img src={item.images[0]} alt="image" />
